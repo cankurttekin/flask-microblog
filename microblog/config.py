23bb803cad9b2e6d7ba12cddc42ee091cb8b8611
cankurttekin <cankurttekin@gmail.com>
2023-11-10 13:35:54 -0500

    pagination

--- microblog/config.py
+++ microblog/config.py
@@ -11,3 +11,4 @@ class Config(object):
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['your-email@example.com']
+	POSTS_PER_PAGE = 15
