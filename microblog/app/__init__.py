fc812d261063a0e8a2b9b34d9ee5f47af8671998
cankurttekin <cankurttekin@gmail.com>
2023-11-11 12:20:23 -0500

    email support & password reset

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
